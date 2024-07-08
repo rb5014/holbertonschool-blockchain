d274c63ed402170a58411e1e4f84f6e4635d3c7d
Romain Bayle <5014@holbertonstudents.com>
2024-07-08 22:05:27 +0200

    ec_verify added key check before signature check

--- crypto/ec_verify.c
+++ crypto/ec_verify.c
@@ -17,5 +17,8 @@ int ec_verify(EC_KEY const *key, uint8_t const *msg, size_t msglen,
 	if (!key || !msg || !sig)
 		return (0);
 
+	if (!EC_KEY_check_key(key))
+		return (0);
+
 	return (ECDSA_verify(0, msg, msglen, sig->sig, sig->len, (EC_KEY *) key));
 }

6af31f8b77eec676808f5668349ef4e93dc200b4
Romain Bayle <5014@holbertonstudents.com>
2024-07-08 17:25:23 +0200

    ec_from_pub fixed

--- crypto/ec_from_pub.c
+++ crypto/ec_from_pub.c
@@ -11,6 +11,8 @@
 EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN])
 {
 	EC_KEY *key;
+	const EC_GROUP *group;
+	EC_POINT *point;
 
 	if (!pub)
 		return (NULL);
@@ -19,8 +21,27 @@ EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN])
 	if (!key)
 		return (NULL);
 
-	if (EC_KEY_oct2key(key, pub, EC_PUB_LEN, NULL) == 0)
+	group = EC_KEY_get0_group(key);
+	if (!group)
+	{
+		EC_KEY_free(key);
 		return (NULL);
+	}
 
+	point = EC_POINT_new(group);
+	if (!point)
+	{
+		EC_KEY_free(key);
+		return (NULL);
+	}
+
+	EC_POINT_oct2point(group, point, pub, EC_PUB_LEN, NULL);
+	if (EC_KEY_set_public_key(key, point) == 0)
+	{
+		EC_KEY_free(key), EC_POINT_free(point);
+		return (NULL);
+	}
+
+	EC_POINT_free(point);
 	return (key);
 }

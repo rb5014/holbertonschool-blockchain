bdfd5161ace6203a74f48e52c62e4cf8090a44c5
Romain Bayle <5014@holbertonstudents.com>
2024-07-29 21:24:20 +0200

    test

--- blockchain/v0.3/transaction/transaction.h
+++ blockchain/v0.3/transaction/transaction.h
@@ -70,20 +70,6 @@ typedef struct unspent_tx_out_s
 } unspent_tx_out_t;
 
 tx_out_t *tx_out_create(uint32_t amount, uint8_t const pub[EC_PUB_LEN]);
-unspent_tx_out_t
-*unspent_tx_out_create(uint8_t block_hash[SHA256_DIGEST_LENGTH],
-					   uint8_t tx_id[SHA256_DIGEST_LENGTH], tx_out_t const *out);
-tx_in_t *tx_in_create(unspent_tx_out_t const *unspent);
-uint8_t *transaction_hash(transaction_t const *transaction,
-						  uint8_t hash_buf[SHA256_DIGEST_LENGTH]);
-sig_t *tx_in_sign(tx_in_t *in, uint8_t const tx_id[SHA256_DIGEST_LENGTH],
-				  EC_KEY const *sender, llist_t *all_unspent);
-transaction_t *transaction_create(EC_KEY const *sender, EC_KEY const *receiver,
-								  uint32_t amount, llist_t *all_unspent);
-int transaction_is_valid(transaction_t const *transaction,
-						 llist_t *all_unspent);
-transaction_t *coinbase_create(EC_KEY const *receiver, uint32_t block_index);
-int coinbase_is_valid(transaction_t const *coinbase, uint32_t block_index);
-void transaction_destroy(transaction_t *transaction);
+
 
 #endif /* TRANSACTION_H */

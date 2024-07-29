c346f2e5fb30ba4f223e7dea3bc399450804877a
Romain Bayle <5014@holbertonstudents.com>
2024-07-29 22:12:26 +0200

    removed memset and memcpy error checking, changed len computation

--- blockchain/v0.3/transaction/tx_out_create.c
+++ blockchain/v0.3/transaction/tx_out_create.c
@@ -5,7 +5,7 @@
  * @amount: amount of the transaction
  * @pub: public key of the transaction receiver
  *
- * Return: Pointer to the created transaction output success,
+ * Return: Pointer to the created transaction output upon success,
  * or NULL on failure
 */
 tx_out_t *tx_out_create(uint32_t amount, uint8_t const pub[EC_PUB_LEN])
@@ -13,20 +13,20 @@ tx_out_t *tx_out_create(uint32_t amount, uint8_t const pub[EC_PUB_LEN])
 	size_t len;
 	tx_out_t *new_tx_out;
 
-	if (amount == 0 || pub == NULL)
+	if (pub == NULL)
 		return (NULL);
 
 	new_tx_out = malloc(sizeof(tx_out_t));
 	if (!new_tx_out)
 		return (NULL);
 
-	/* Init */
+	/* Init members to 0*/
+	memset(new_tx_out, 0, sizeof(*new_tx_out));
+	/* Update values */
 	new_tx_out->amount = amount;
-	if (!memcpy(new_tx_out->pub, pub, EC_PUB_LEN) ||
-	    !memset(new_tx_out->hash, 0, SHA256_DIGEST_LENGTH))
-		return (NULL);
+	memcpy(new_tx_out->pub, pub, sizeof(new_tx_out->pub));
 
-	len = sizeof(new_tx_out->amount) + sizeof(new_tx_out->pub);
+	len = sizeof(*new_tx_out) - sizeof(new_tx_out->hash);
 
 	sha256((int8_t const *)new_tx_out, len, new_tx_out->hash);
 

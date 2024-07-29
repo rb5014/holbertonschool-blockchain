707bae8fd78cfb576b608769280b43371be729d7
Romain Bayle <5014@holbertonstudents.com>
2024-07-29 21:23:04 +0200

    betty fixed

--- blockchain/v0.3/blockchain.h
+++ blockchain/v0.3/blockchain.h
@@ -101,13 +101,15 @@ extern block_t const _genesis;
 
 blockchain_t *blockchain_create(void);
 
-block_t *block_create(block_t const *prev, int8_t const *data, uint32_t data_len);
+block_t *block_create(block_t const *prev, int8_t const *data,
+					  uint32_t data_len);
 
 void block_destroy(block_t *block);
 
 void blockchain_destroy(blockchain_t *blockchain);
 
-uint8_t *block_hash(block_t const *block, uint8_t hash_buf[SHA256_DIGEST_LENGTH]);
+uint8_t *block_hash(block_t const *block,
+				    uint8_t hash_buf[SHA256_DIGEST_LENGTH]);
 
 int blockchain_serialize(blockchain_t const *blockchain, char const *path);
 
